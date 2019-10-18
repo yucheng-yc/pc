3c96bb531d400455480bc8bd822a319f677a7c85
yc <318771301@qq.com>
2019-10-18 17:58:56 +0800

    首页

---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login" :style="{background:bgUrl}">
+
+    </div>
+</template>
+
+<style scoped>
+    .login {
+        height: 689px;
+        width:100%;
+    }
+
+</style>
+
+<script>
+export default {
+    props:{
+        type:String,
+        default:"url(@/assets/images/bg_login.jpg) no repect cover"
+    }
+}
+</script>
